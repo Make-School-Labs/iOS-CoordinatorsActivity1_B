763c1825a6a48e1e1981d4180913c7d73517dbac
VanderDev1 <thomasv@vandervision.com>
2019-04-24 15:18:35 -0700

    initial upload

---
+++ iOS-CoordinatorsActivity1_B/iOS-CoordinatorsActivity1_B/Coordinators/MainCoordinator.swift
@@ -0,0 +1,38 @@
+//
+//  MainCoordinator.swift
+//  iOS-CoordinatorsActivity1_B
+//
+//  Created by Thomas Vandegriff on 4/24/19.
+//  Copyright © 2019 Make School. All rights reserved.
+//
+
+import UIKit
+
+class MainCoordinator: Coordinator {
+    
+    var childCoordinators = [Coordinator]()
+    var navigationController: UINavigationController
+    
+    init(navigationController: UINavigationController) {
+        self.navigationController = navigationController
+    }
+    
+    func start() {
+        let vc = ViewController.instantiate()
+        vc.coordinator = self
+        navigationController.pushViewController(vc, animated: true)
+    }
+    
+      //TODO: Create
+    func buySubscription() {
+        let vc = BuyViewController.instantiate()
+        vc.coordinator = self
+        navigationController.pushViewController(vc, animated: true)
+    }
+    
+    func createAccount() {
+        let vc = CreateAccountViewController.instantiate()
+        vc.coordinator = self
+        navigationController.pushViewController(vc, animated: true)
+    }
+}
